3a76c4e4ad4d9c1003a0ce363d7ad373114d9dc2
t1m4 <731ruslan00@mail.ru>
2021-01-07 13:32:13 +0300

    Create model for Calculation and Count. And register them

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Calculations, Counts
+
+admin.site.register(Calculations)
+admin.site.register(Counts)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
+class Calculations(models.Model):
+    p_class = models.CharField(max_length=32)
+    sex = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0)])
+    subling = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(25)])
+    parents = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(2)])
+    fare = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(512)])
+    answers = models.BooleanField(default=False)
+
+class Counts(models.Model):
+    enter = models.IntegerField(validators=[MinValueValidator(0)])
