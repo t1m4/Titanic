cb0680de746fc12af41f7fffd2cca79364493a8c
t1m4 <731ruslan00@mail.ru>
2021-01-08 18:09:38 +0300

    Add save best weight and loss

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -5,8 +7,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.utils.decorators import method_decorator
 from django.views import View
-
-from project import start, datas_train, targets_train, weight, learning_rate, loops, sigma
+import numpy as np
+from project import start, datas_train, targets_train, weight, learning_rate, loops, sigma, write_file, read_file
 
 
 @method_decorator(login_required, name='dispatch')
@@ -14,7 +16,7 @@ class StartView(View):
     def get(self, request, *args, **kwargs):
         try:
             res = start(datas_train, targets_train, weight, learning_rate, loops, sigma=sigma)
-            print(res)
+            write_file(res)
             return HttpResponse('ok', status=200)
         except Exception as e:
             return HttpResponse(e, status=200)
