aa5c1ec7b60acfd775fe343b5212184a1f94e451
t1m4 <731ruslan00@mail.ru>
2021-01-10 12:26:20 +0300

    Change label

--- core/forms.py
+++ core/forms.py
@@ -6,7 +6,7 @@ from core.models import Calculations
 class User(forms.ModelForm):
     age = forms.IntegerField(min_value=0, max_value=150)
     siblings = forms.IntegerField(min_value=0, max_value=25, label="Sibling/Spouses aboard")
-    parents = forms.IntegerField(min_value=0,max_value=2, label="Parents/Children")
+    parents = forms.IntegerField(min_value=0,max_value=2, label="Parents/Children aboard")
     fare = forms.IntegerField(min_value=0, max_value=512)
     class Meta:
         model = Calculations
